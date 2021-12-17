e042cb28be423b8262004ed7f2c35772dcd397b5
Tom Farrell <tomxfarrell@gmail.com>
2021-12-17 17:00:04 -0500

    Latest

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA14xcELq2lr3IvR1Oa_WsJK8PfPoueFKc",
+  authDomain: "iamtomfarrellsite.firebaseapp.com",
+  projectId: "iamtomfarrellsite",
+  storageBucket: "iamtomfarrellsite.appspot.com",
+  messagingSenderId: "816924721496",
+  appId: "1:816924721496:web:8aa4b6a64db97a2759e52d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
