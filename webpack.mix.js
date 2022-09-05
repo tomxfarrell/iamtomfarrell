0b8deac8abaabad9ce8e6ad900d65c2c58c8ca24
Tom Farrell <tomxfarrell@gmail.com>
2022-09-05 11:04:49 -0400

    test

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,14 @@
-const mix = require('laravel-mix');
-require('laravel-mix-jigsaw');
+const mix = require("laravel-mix");
+require("laravel-mix-jigsaw");
 
 mix.disableSuccessNotifications();
-mix.setPublicPath('source/assets/build');
+mix.setPublicPath("source/assets/build");
 
-mix.jigsaw()
-    .js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
-    .options({
-        processCssUrls: false,
-    })
-    .version();
+mix
+  .jigsaw()
+  .js("source/_assets/js/main.js", "js")
+  .sass("source/_assets/sass/main.scss", "css")
+  .options({
+    processCssUrls: false,
+  })
+  .version();
