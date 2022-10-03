22b43540c819dde3df1cf7692edb730e4b74b9fc
Tom Farrell <tomxfarrell@gmail.com>
2022-10-03 10:56:41 -0400

    test

--- source/_assets/js/heightFix.js
+++ source/_assets/js/heightFix.js
@@ -2,6 +2,7 @@ const setHeight = () => {
   document.getElementById("hero").style.minHeight = window.innerHeight + "px";
 };
 
+let test123;
 // define mobile screen size:
 
 let deviceWidth = window.matchMedia("(max-width: 415px)");
